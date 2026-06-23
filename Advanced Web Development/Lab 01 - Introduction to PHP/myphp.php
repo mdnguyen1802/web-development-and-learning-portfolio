b9891c321c3e77a248adf42ff0d268d5237594a0
Minh Duc Nguyen <mdnguyen1802@gmail.com>
2026-06-23 10:48:21 +0700

    Create myphp.php

---
+++ Advanced Web Development/Lab 01 - Introduction to PHP/myphp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-AU">
+<head>
+    <title>My First PHP Webpage</title>
+    <meta charset = "utf-8">
+    <meta name = "description" content = "Web development">
+    <meta name = "keywords" content = "HTML, CSS, JavaScript">
+    <meta name = "author" content = "Minh Duc Nguyen">
+</head>
+
+<body>
+    <h1>Advanced Web Programming - Lab 1</h1>
+
+    <p>Can I create a .php file that do not contain any PHP scripts?</p>
+
+    <p>
+        Yes, but this should be avoided, so that the server does not do any
+        unnecessary parsing and processing.
+    </p>
+</body>
+</html>
